81dbe14bea784eb20fc4060827b8ec5fa70f2783
OriahWebStudios <oriahwebstudios@outlook.com>
2025-06-06 19:47:38 +0200

    feat: Add Course class to represent course entities

--- StudentManagementSystem/Models/Course.cs
+++ StudentManagementSystem/Models/Course.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Define a namespace to organize related classes and types
 namespace StudentManagementSystem.Models
 {
-    internal class Course
+    // Define a public class named Course to represent a course entity
+    public class Course
     {
+        // Define a public property to store the unique identifier of the course
+        public int CourseId { get; set; }
+
+        // Define a public property to store the name of the course
+        public string CourseName { get; set; }
+
+        // Define a constructor to initialize a new Course object
+        // This constructor takes two parameters: courseId and courseName
+        public Course(int courseId, string courseName)
+        {
+            // Assign the constructor parameters to the corresponding properties
+            CourseId = courseId;
+            CourseName = courseName;
+        }
+
+        // Override the ToString method to provide a human-readable representation of the Course object
+        // This method returns a string that includes the course ID and course name
+        public override string ToString()
+        {
+            // Use string interpolation to format the string with the course ID and course name
+            return $"Course ID: {CourseId} - {CourseName}";
+        }
     }
 }
